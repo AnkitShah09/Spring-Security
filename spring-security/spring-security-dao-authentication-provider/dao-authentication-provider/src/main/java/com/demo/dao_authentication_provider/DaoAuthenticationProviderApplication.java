946915e7d050ddd1d571a39d464a45ccfa371742
AnkitShah9 <ankitshah123440@gmail.com>
2024-08-20 15:40:25 +0530

    first commit

---
+++ spring-security/spring-security-dao-authentication-provider/dao-authentication-provider/src/main/java/com/demo/dao_authentication_provider/DaoAuthenticationProviderApplication.java
@@ -0,0 +1,13 @@
+package com.demo.dao_authentication_provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaoAuthenticationProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaoAuthenticationProviderApplication.class, args);
+	}
+
+}
